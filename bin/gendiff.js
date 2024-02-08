bf252d03568e88b7c44138d4f521744cd5a2a816
rizzd <evgeniy_cheraev@vk.com>
2024-02-08 22:02:23 +0300

    step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,12 @@ const program = new Command();
 
 program
   .version('0.1.0')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .action((filepath1, filepath2) => {
+	console.log(filepath1, filepath2)
+  });
 
-program.parse(process.argv);
+program.parse();
