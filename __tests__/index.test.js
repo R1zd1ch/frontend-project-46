c5ff1b7f3513cf1480dba28ddb0d56a811260fd3
rizzd <evgeniy_cheraev@vk.com>
2024-02-13 20:56:57 +0300

    step 8

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -11,20 +11,72 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 
 describe('Stylish Tests', () => {
   test('filejson', () => {
-    const filename1 = getFixturePath('file1.json');
-    const filename2 = getFixturePath('file2.json');
-    const resultname = getFixturePath('file_result.txt');
+    const filepath1 = getFixturePath('file1.json');
+    const filepath2 = getFixturePath('file2.json');
+    const resultname = getFixturePath('expectedStylish.txt');
     const result = readFileSync(resultname, 'utf8');
-    expect(gendiff(filename1, filename2)).toEqual(result);
+    expect(gendiff(filepath1, filepath2)).toEqual(result);
   });
 });
 
-describe('Flat YAML Stylish Test', () => {
-  test('filejson', () => {
-    const filename1 = getFixturePath('file1.yml');
-    const filename2 = getFixturePath('file2.yml');
-    const resultname = getFixturePath('file_result.txt');
+describe('YML Stylish Test', () => {
+  test('fileyml', () => {
+    const filepath1 = getFixturePath('file1.yml');
+    const filepath2 = getFixturePath('file2.yml');
+    const resultname = getFixturePath('expectedStylish.txt');
     const result = readFileSync(resultname, 'utf8');
-    expect(gendiff(filename1, filename2)).toEqual(result);
+    expect(gendiff(filepath1, filepath2)).toEqual(result);
+  });
+});
+
+describe('YAML Stylish Test', () => {
+  test('fileyaml', () => {
+    const filepath1 = getFixturePath('file1.yaml');
+    const filepath2 = getFixturePath('file2.yaml');
+    const resultname = getFixturePath('expectedStylish.txt');
+    const result = readFileSync(resultname, 'utf8');
+    expect(gendiff(filepath1, filepath2)).toEqual(result);
+  });
+});
+
+describe('json plain Test', () => {
+  test('filejson', () => {
+    const filepath1 = getFixturePath('file1.json');
+    const filepath2 = getFixturePath('file2.json');
+    const resultname = getFixturePath('expectedPlain.txt');
+    const result = readFileSync(resultname, 'utf8');
+    expect(gendiff(filepath1, filepath2, 'plain')).toEqual(result);
+  });
+});
+
+describe('YML plain Test', () => {
+  test('fileyml', () => {
+    const filepath1 = getFixturePath('file1.yml');
+    const filepath2 = getFixturePath('file2.yml');
+    const resultname = getFixturePath('expectedPlain.txt');
+    const result = readFileSync(resultname, 'utf8');
+    expect(gendiff(filepath1, filepath2, 'plain')).toEqual(result);
+  });
+});
+
+describe('YAML plain Test', () => {
+  test('fileyaml', () => {
+    const filepath1 = getFixturePath('file1.yaml');
+    const filepath2 = getFixturePath('file2.yaml');
+    const resultname = getFixturePath('expectedPlain.txt');
+    const result = readFileSync(resultname, 'utf8');
+    expect(gendiff(filepath1, filepath2, 'plain')).toEqual(result);
+  });
+});
+
+describe('Errors tests', () => {
+  test('Unsupported format type', () => {
+    const filepath1 = getFixturePath('file1.yaml');
+    const filepath2 = getFixturePath('file2.yaml');
+    const badpath1 = getFixturePath('file1.txt');
+    const badpath2 = getFixturePath('file2.txt');
+    const extErr = new Error('Unsupported file extention (.txt)! [Supported: .json, .yml, .yaml]');
+    expect(() => gendiff(filepath1, badpath2)).toThrow(extErr);
+    expect(() => gendiff(badpath1, filepath2)).toThrow(extErr);
   });
 });
